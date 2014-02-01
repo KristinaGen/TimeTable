3337b35f9fea64b94f9d3c1645d5d6ebc1aa28da
KristinaGen <kr.genchikova@gmail.com>
2014-02-02 00:57:21 +0400

    create some classes in logic

---
+++ Logic/BusFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class BusFactory:IFactory
+    {
+        public List<ITransport> createListRoute(IFactory factory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Logic/Train.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Train : ITransport
+    {
+        public int NumberTrain { set; get; }
+        public string TypeTrain { set; get; }
+
+    }
+}
